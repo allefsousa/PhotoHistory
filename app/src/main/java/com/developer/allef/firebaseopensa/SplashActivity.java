ba294ecacfe634433d455e79a7ea599b905f760d
Allef Sousa <allefsousa_1@hotmail.com>
2017-03-25 10:52:03 -0300

    init project

---
+++ app/src/main/java/com/developer/allef/firebaseopensa/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.developer.allef.firebaseopensa;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mAuth = FirebaseAuth.getInstance();
+
+        // verificando autenticação Anonyma.
+        mAuth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    // TODO: Fail ......
+                    return;
+                }
+                //startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
